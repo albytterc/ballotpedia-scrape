e53a725cb1cfbc967754304b2049ceec214e7ae3
Gustavo-Soto-Reyes <83844074+Gustavo-Soto-Reyes@users.noreply.github.com>
2022-10-17 17:23:49 -0400

    display races and update color

--- components/RacesBox.tsx
+++ components/RacesBox.tsx
@@ -1,35 +1,34 @@
-import { View, Text } from "react-native";
-import { Box, Heading, VStack } from "native-base";
+import { View } from "react-native";
+import { Box, Heading, VStack, Text } from "native-base";
 import React from "react";
 
 const RacesBox = ({ text }) => {
   return (
     <Box
-      style={{ width: "40%", aspectRatio: 1 }}
-      bg="#2708A0"
+      style={{ width: "45%", aspectRatio: 1 }}
+      height="100px"
+      bg="#E8E8E8"
       p="4"
       shadow={2}
       borderRadius={20}
       borderWidth="2px"
+      borderColor={"rgba(162,156,244,255)"}
       alignItems={"center"}
+      justifyContent={"center"}
       display={"flex"}
       flexDirection={"row"}
       marginBottom={"10px"}
     >
-      <Box width={"auto"} alignItems={"center"}>
-        <VStack>
-          <Heading size="xl" color="#FFFFFF">
-            {text}
-          </Heading>
-          <Heading
-            size="sm"
-            alignSelf={"flex-end"}
-            marginTop="1rem"
-            color="#FFFFFF"
-          >
-            Date
-          </Heading>
-        </VStack>
+      <Box display={"flex"} justifyContent={"center"} textAlign={"center"}>
+        <Text
+          color="#5c5a5b"
+          fontWeight={"600"}
+          textAlign={"center"}
+          display={"flex"}
+          justifyContent={"center"}
+        >
+          {text}
+        </Text>
       </Box>
     </Box>
   );
--- src/pages/Races/Races.tsx
+++ src/pages/Races/Races.tsx
@@ -1,29 +1,78 @@
 import { View, Text } from "react-native";
-import { Box, Heading, VStack } from "native-base";
-import React from "react";
+import { Box, FlatList, Heading, VStack, SectionList } from "native-base";
+import React, { useState, useEffect } from "react";
 import RacesBox from "../../../components/RacesBox";
 
+const BASE_URL = "https://www.googleapis.com/civicinfo/v2";
+const endpoint = "/voterinfo";
+const API_KEY = "AIzaSyDjmpk9KkK8jE1MUQLbxXRohVDnnlA2r8Y";
+const address = "5019 Lonnie D Aldridge Rd Monroe, NC 28112";
+const electionId = "8000";
+
 const Races = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  //   const data: string[] = ["hello ", "bitch"];
+  //   const [data, setData] = useState(["hello ", "bitch"]);
+
+  useEffect(() => {
+    fetch(
+      BASE_URL +
+        endpoint +
+        "?address=" +
+        address +
+        "&electionId=" +
+        electionId +
+        "&key=" +
+        API_KEY
+    )
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => alert(error))
+      .finally(() => setLoading(false));
+    console.log(typeof data);
+    console.log(data);
+  }, []);
+  const listItems = [];
+  if (data.contests != undefined) {
+    console.log(data.contests);
+    console.log(data.contests.length);
+    for (var i = 0; i < data.contests.length; i++) {
+      listItems.push(<RacesBox text={data.contests[i].ballotTitle} />);
+    }
+    console.log("Loading is " + isLoading);
+  } else {
+    console.log("Undefined data");
+    console.log("Loading is " + isLoading);
+  }
+
   return (
     <>
-      <Heading>Races</Heading>
-      <Box
-        display={"flex"}
-        flexDirection={"row"}
-        justifyContent={"space-evenly"}
-        flexWrap={"wrap"}
-      >
-        <RacesBox text={"Hello"} />
-        <RacesBox text={"Hello"} />
-        <RacesBox text={"Hello"} />
-        <RacesBox text={"Hello"} />
-        <RacesBox text={"Hello"} />
-        <RacesBox text={"Hello"} />
-        <RacesBox text={"Hello"} />
-        <RacesBox text={"Hello"} />
-        <RacesBox text={"Hello"} />
-        <RacesBox text={"Hello"} />
-      </Box>
+      <SectionList
+        background={"#e4e3f1"}
+        marginY={2}
+        ListFooterComponent={
+          <>
+            <Heading
+              marginBottom={"0.5rem"}
+              marginTop={"0.5rem"}
+              marginLeft={"0.5rem"}
+              color={"black"}
+            >
+              Races
+            </Heading>
+            <Box
+              display={"flex"}
+              flexDirection={"row"}
+              justifyContent={"space-evenly"}
+              flexWrap={"wrap"}
+            >
+              {listItems}
+            </Box>
+          </>
+        }
+        sections={[]}
+      />
     </>
   );
 };
