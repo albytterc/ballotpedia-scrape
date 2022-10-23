a522c7ed9c8f71f566cf42bbe769b96a765cbc73
Gustavo-Soto-Reyes <83844074+Gustavo-Soto-Reyes@users.noreply.github.com>
2022-10-23 01:54:34 -0400

    candidate parties

--- components/CandidateBox.tsx
+++ components/CandidateBox.tsx
@@ -1,8 +1,21 @@
 import { View } from "react-native";
 import React from "react";
-import { Box, Link, Text } from "native-base";
+import { Box, Heading, Link, Text } from "native-base";
+import { border } from "native-base/lib/typescript/theme/styled-system";
 
-const CandidateBox = ({ text, navigation, data }) => {
+const CandidateBox = ({ navigation, candidateData }) => {
+  let partyColor = "black";
+  if (candidateData.party) {
+    if (candidateData.party.toUpperCase().match("REPUBLICAN")) {
+      partyColor = "#FF0000";
+    } else if (candidateData.party == "DEMOCRATIC") {
+      partyColor = "#0000FF";
+    } else if (candidateData.party == "LIBERTARIAN") {
+      partyColor = "#FFD700";
+    } else if (candidateData.party == "GREEN") {
+      partyColor = "#007500";
+    }
+  }
   return (
     <Link
       style={{ width: "45%", aspectRatio: 1 }}
@@ -12,25 +25,38 @@ const CandidateBox = ({ text, navigation, data }) => {
       shadow={2}
       borderRadius={20}
       borderWidth="2px"
-      borderColor={"rgba(162,156,244,255)"}
+      // borderColor={"rgba(162,156,244,255)"}
+      borderColor={partyColor}
+      // borderColor={"#FFFFFF"}
       alignItems={"center"}
       justifyContent={"center"}
       display={"flex"}
       flexDirection={"row"}
       marginBottom={"10px"}
-      // onPress={() => navigation.navigate("Candidates")}
+      onPress={() => navigation.navigate("Races")}
       mt="8"
     >
       <Box>
         <Box display={"flex"} justifyContent={"center"} textAlign={"center"}>
-          <Text
-            color="#5c5a5b"
+          <Heading
+            size={"md"}
             fontWeight={"600"}
             textAlign={"center"}
             display={"flex"}
             justifyContent={"center"}
           >
-            {/* {text} */}
+            {candidateData.name}
+          </Heading>
+
+          <Text
+            // style={{ color: partyColor }}
+            color="#5c5a5b"
+            fontWeight={"400"}
+            textAlign={"center"}
+            display={"flex"}
+            justifyContent={"center"}
+          >
+            {candidateData.party}
           </Text>
         </Box>
       </Box>
--- src/pages/Candidates/Candidates.tsx
+++ src/pages/Candidates/Candidates.tsx
@@ -1,24 +1,52 @@
 import { View } from "react-native";
 import React from "react";
-import { Box, Heading, VStack, Text, Pressable, Link } from "native-base";
+import {
+  Box,
+  Heading,
+  VStack,
+  Text,
+  Pressable,
+  Link,
+  SectionList,
+} from "native-base";
 import CandidateBox from "../../../components/CandidateBox";
 
 const Candidates = ({ route, navigation }) => {
   const { data } = route.params;
   console.log("in Candidates");
   console.log(data);
-  return (
-    <Box>
-      <Text>These are the candidates running for office:</Text>
-      {data.ballotTitle}
-      <Text></Text>
-
-      {data.candidates.map((map) => (
-        <Text>
-          {map.name} is running as a {map.party}
-        </Text>
-      ))}
-    </Box>
+  return data.candidates ? (
+    <>
+      <SectionList
+        background={"#e4e3f1"}
+        marginY={2}
+        ListFooterComponent={
+          <>
+            <Heading>Candidates</Heading>
+            {/* <Text>These are the candidates running for:</Text> */}
+            <Heading size="sm" textAlign={"center"} marginTop={"1rem"}>
+              {data.ballotTitle}
+            </Heading>
+            <Box
+              display={"flex"}
+              flexDirection={"row"}
+              justifyContent={"space-evenly"}
+              flexWrap={"wrap"}
+            >
+              {data.candidates.map((map) => (
+                // <Text>
+                <CandidateBox navigation={navigation} candidateData={map} />
+              ))}
+            </Box>
+          </>
+        }
+        sections={[]}
+      />
+    </>
+  ) : (
+    <>
+      <Text>No Candidates for this position Sowwy</Text>
+    </>
   );
 };
 
