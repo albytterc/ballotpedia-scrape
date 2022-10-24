bbeb3c5f35bece91461452a54579dde6f3d3e05c
Mert Ozbay <mert.ozbay@emory.edu>
2022-10-24 10:36:24 -0400

    added maps api key

--- config.ts
+++ config.ts
@@ -1,4 +1,5 @@
 export default {
     API_KEY : "AIzaSyCVdgz-mQ4wpNn3-4CNrQ9MQqaKpNEp1rs",
-    address : "5019 Lonnie D Aldridge Rd Monroe, NC 28112"
+    address : "5019 Lonnie D Aldridge Rd Monroe, NC 28112",
+    MAPS_API_KEY: 'AIzaSyD8yO4iSqHH7ZhrYlmugsA7nNrGGvIMVCc'
 }
