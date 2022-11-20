650c690973cba1ca5d33549b940d9d59a88293dc
Albert Terc <albytterc@protonmail.com>
2022-11-20 17:17:06 -0500

    Removing unnecessary dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,16 @@
 import express, { NextFunction, Request, Response } from "express";
-import config from "config";
 import helmet from "helmet";
 import compression from 'compression';
 import morgan from 'morgan';
 
 import routes from "./routes";
 
-const api = process.env.API_URL;
 const app = express();
 
 
 app.use(compression())
 app.use(helmet());
-app.use(morgan('dev'));
+    app.use(morgan('dev'));
 app.use(express.json());
 
 routes(app)
