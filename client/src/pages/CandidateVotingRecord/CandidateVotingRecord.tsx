0f5b7d8c52dce6486242b6a413b82c40794dda1f
Jason Ji <jijason2018@gmail.com>
2022-12-04 01:06:41 -0500

    voting record finished

--- client/src/pages/CandidateVotingRecord/CandidateVotingRecord.tsx
+++ client/src/pages/CandidateVotingRecord/CandidateVotingRecord.tsx
@@ -8,66 +8,84 @@ import {
   Pressable,
   Link,
   SectionList,
+  Spinner,
 } from "native-base";
 import {WebView} from 'react-native-webview';
 
 const CandidateVotingRecord= ({ route, navigation }) => {
-  const candidate_name = route.params.candidateName;
-  console.log(candidate_name);
-  const heading = "View " + candidate_name + "'s voting record on Vote Smart";
-  const instruction = "1. Search for the candidate " + candidate_name + "\n2. Choose the category 'VOTES'\n3. Scroll down to see " + candidate_name + "'s previous voting record";
+    let candidate_name = route.params.candidate.name;
+
+    const heading = "View " + candidate_name + "'s voting record on Vote Smart";
+
+    // const BASE_URL = "https://ballotpedia-api.cyclic.app/votesmart-api/";
+    const BASE_URL = "http://localhost:3000/votesmart-api/";
+
+    const [isLoading, setLoading] = useState(true);
+    const [candidateURL, setCandidateURL] = useState({
+        url: "",
+    });
 
 
-  const BASE_URL = "https://ballotpedia-api.cyclic.app`/votesmart-api/";
-
-  const [isLoading, setLoading] = useState(true);
-  const [candidateID, setCandidateID] = useState({
-    id: ""
-  });
-
-  const fetchData = () => {
-        fetch(BASE_URL + candidate_name)
-        .then((response) => response.json())
-        .then((json) => setCandidateID(json))
-        .catch((error) => console.error(error))
-        .finally(() => setLoading(false));
-    }
+    const fetchData = () => {
+            fetch(BASE_URL + candidate_name)
+            .then((response) => response.json())
+            .then(async (json) => setCandidateURL(json))
+            .catch((error) => console.error(error))
+            .then(() => console.log(candidateURL))
+            .finally(() => setLoading(false));
+        }
 
     useEffect(() => {
         fetchData();
     }, []);
 
-  const url = "https://justfacts.votesmart.org/candidate/key-votes/" + candidateID.id + "/" + "";
 
-
+    const renderItem = (item: any) => {
+        return (
+            <Box>
+                 {!isLoading ? 
+                 (
+                    <View>
+                        <Heading
+                        size={"lg"}
+                        textAlign={"center"}
+                        marginTop={"1rem"}
+                        marginBottom={"1rem"}
+                        >
+                        {heading}
+                        </Heading>
+                        <View style={{width: "90%", height:"90%", marginLeft:"5%"}}>
+                            <WebView
+                                source={{uri: item.url}}
+                                // onLoad={console.log('Loaded!')}
+                            />
+                        </View> 
+                    </View>
+                 )
+                : (
+                    <View>
+                        <Heading
+                        size={"lg"}
+                        textAlign={"center"}
+                        marginTop={"1rem"}
+                        marginBottom={"1rem"}
+                        >
+                            {heading}
+                        </Heading>
+                        <Spinner size="sm" />
+                    </View>
+                    
+                )}           
+            </Box>   
+        )
+    }
 
 
 
   return (
-    <Box>
-        <Heading
-            size={"lg"}
-            textAlign={"center"}
-            marginTop={"1rem"}
-        >
-            {heading}
-        </Heading>
-        <Heading
-            size={"sm"}
-            textAlign={"center"}
-            marginTop={"1rem"}
-            marginBottom={"1rem"}
-        >
-            {instruction}
-        </Heading>
-        <View style={{width: "90%", height:"90%", marginLeft:"5%"}}>
-            <WebView
-                source={{uri: url}}
-                onLoad={console.log('Loaded!')}
-            />
-        </View>
-        
-    </Box>
+    <View>
+        {renderItem(candidateURL)}
+    </View>
   );
 };
 
